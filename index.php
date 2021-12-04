cefb3a491bf024c18158c5d98c29442a0cda257a
Tor Inge Brekka <tor_ib@hotmail.com>
2021-12-05 00:55:14 +0100

    checkpointq

--- index.php
+++ index.php
@@ -7,15 +7,26 @@
     <body>
         <?php
             session_start();
-            if(!isset($_SESSION['bruker']['innlogget']) || $_SESSION['bruker']['innlogget'] !== true) {
-                header("Location: login.php");
-                exit();
+            function sjekkInnlogget() {
+                if(!isset($_SESSION['bruker']['innlogget']) || $_SESSION['bruker']['innlogget'] !== true) {
+                    header("Location: login.php");
+                    exit();
+                }
             }
         ?>
         <h1>Neo Ungdomsklubb - medlemsoversikt</h1>
         <br><br>
         <p>Registrer nytt medlem <a href="registrermedlem.php">her.</a></p>
-
-        
+        <p style="align-content: right">
+            <form>
+                <input type="button" name="loggut" value="Logg ut">
+            </form>
+        </p>
+        <?php
+            if(isset($_POST['loggut'])) {
+                $_SESSION['bruker']['innlogget'] = false;
+                sjekkInnlogget();
+            }
+        ?>
     </body>
 </html>
