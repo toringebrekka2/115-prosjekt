7351c700b7b167d2f26b2856195522e826e81ff9
Banjorama <59700869+Banjorama@users.noreply.github.com>
2021-12-07 16:31:18 +0100

    Fikset visning fullstendig ;)

--- visning.php
+++ visning.php
@@ -9,7 +9,7 @@
         <h1> Medlemsoversikt </h1>
         <?php include 'navbar.php'; ?>
         <form action="visning.php" method="post">
-        <label for="kontigent">Sorter etter kontigent:</label>
+        <label for="kontigent">Sortér etter kontigent:</label>
         <select name="kontigent" id="kontigent">
         <option value="Tom1"></option>    
         <option value="Betalt">Betalt</option>
@@ -20,9 +20,9 @@
 </form>
 
 <form action="visning.php" method="post">
-        <label for="medlemsiden">Sorter etter innemeldingsdato:</label>
+        <label for="medlemsiden">Sortér etter innemeldingsdato:</label>
         <select name="medlemsiden" id="medlemsiden">
-        <option value="Tom2"></option>    
+        <option value="Tom2"></option>
         <option value="Synkende">Synkende</option>
         <option value="Økende">Økende</option>
     </select>
@@ -30,6 +30,7 @@
     <input type="submit" name = "submit2" value="Sortér">
 </form>
     <?php
+
     if(!isset($_SESSION['bruker']['innlogget']) || $_SESSION['bruker']['innlogget'] !== true) {
         header("Location: login.php");
         exit();
@@ -42,66 +43,74 @@
     $check = "SELECT id, fornavn, etternavn, adresse, postnr, mobilnr, epost, fødselsdato, kjønn, interesser, medlemsiden, kontigentstatus FROM medlem";
     $sql = $check;
 
-    if (isset($_POST['submit2']) && !isset($_POST['submit1']){
-        if ($_POST['medlemsiden'] == 'Synkende'){
-        $sql .= " ORDER BY medlemsiden DESC";
-        }
-        if ($_POST['medlemsiden'] == 'Økende'){
-        $sql .= " ORDER BY medlemsiden ASC";
-        }
-        } else $sql = $check;
-   
-    if (isset($_POST['submit1']) && !isset($_POST['submit2'])){
-        if ($_POST['kontigent'] == 'Betalt'){
-        $sql .= " ORDER BY kontigentstatus";
-        echo $sql;
-        }
-        if ($_POST['kontigent'] == 'IkkeBetalt'){
-        $sql .= " ORDER BY kontigentstatus DESC"; 
-        echo $sql;
-        }
-        } else $sql = $check;
+    switch(!empty($_POST)){
+           
+        case @$_POST['kontigent'] == "Betalt":
+            $sql .= " ORDER BY kontigentstatus";
+            makeTable ($conn, $sql);
+        break;
+
+        case @$_POST['kontigent'] == "IkkeBetalt":
+            $sql .= " ORDER BY kontigentstatus DESC";
+            makeTable ($conn, $sql);
+        break;
+
+        case @$_POST['medlemsiden'] == "Økende":
+            $sql .= " ORDER BY medlemsiden ASC";
+            makeTable ($conn, $sql);
+        break;
+
+        case @$_POST['medlemsiden'] == "Synkende":
+            $sql .= " ORDER BY medlemsiden DESC";
+            makeTable ($conn, $sql);  
+        break;
+        
+        case @$_POST['medlemsiden'] == "Tom2" || @$_POST['kontigent'] == "Tom1":
+            makeTable ($conn, $sql);  
+        break;
+
+      }
 
 
+    function makeTable ($conn, $sql) {
+        $result = $conn->query($sql);
+        echo "
+        <table>
+    <tr>
+        <th>ID</th>
+        <th>Fornavn</th>
+        <th>Etternavn</th>
+        <th>Addresse</th>
+        <th>Postnummer</th>
+        <th>Mobilnummer</th>
+        <th>E-postadresse</th>
+        <th>Fødselsdato</th>
+        <th>Kjønn</th>
+        <th>Interesser</th>
+        <th>Medlem siden</th>
+        <th>Kontigentstatus</th>
+    </tr>";
 
-    $result = $conn->query($sql);
-    echo "
-    <table>
-  <tr>
-    <th>ID</th>
-    <th>Fornavn</th>
-    <th>Etternavn</th>
-    <th>Addresse</th>
-    <th>Postnummer</th>
-    <th>Mobilnummer</th>
-    <th>E-postadresse</th>
-    <th>Fødselsdato</th>
-    <th>Kjønn</th>
-    <th>Interesser</th>
-    <th>Medlem siden</th>
-    <th>Kontigentstatus</th>
-</tr>";
-
-    while($row = $result->fetch_assoc()) {
-echo
-"<tr>
-<td>" . $row['id'] . "</td>
-<td>" . $row['fornavn'] . "</td>
-<td>" . $row['etternavn'] . "</td>
-<td>" . $row['adresse'] . "</td>
-<td>" . $row['postnr'] . "</td>
-<td>" . $row['mobilnr'] . "</td>
-<td>" . $row['epost'] . "</td>
-<td>" . $row['fødselsdato'] . "</td>
-<td>" . $row['kjønn'] . "</td>
-<td>" . $row['interesser'] . "</td>
-<td>" . $row['medlemsiden'] . "</td>
-<td>" . $row['kontigentstatus'] . "</td>
-</tr>";
-
-
+        while($row = $result->fetch_assoc()) {
+    echo
+    "<tr>
+        <td>" . $row['id'] . "</td>
+        <td>" . $row['fornavn'] . "</td>
+        <td>" . $row['etternavn'] . "</td>
+        <td>" . $row['adresse'] . "</td>
+        <td>" . $row['postnr'] . "</td>
+        <td>" . $row['mobilnr'] . "</td>
+        <td>" . $row['epost'] . "</td>
+        <td>" . $row['fødselsdato'] . "</td>
+        <td>" . $row['kjønn'] . "</td>
+        <td>" . $row['interesser'] . "</td>
+        <td>" . $row['medlemsiden'] . "</td>
+        <td>" . $row['kontigentstatus'] . "</td>
+    </tr>";
     }
-    echo "</table>;"
+
+    echo "</table>;";
+}
 ?>
     </body>
 </html>
